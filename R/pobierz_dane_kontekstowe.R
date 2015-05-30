eff70cc0af4993f48eddaf808910ef91c59b160b
tzoltak <t.zoltak@ibe.edu.pl>
2015-05-30 10:06:18 +0200

    Dopracowanie warunków na populację w pobierz_dane_kontekstowe(); dodanie pobierz_wyniki_surowe()

--- R/pobierz_dane_kontekstowe.R
+++ R/pobierz_dane_kontekstowe.R
@@ -1,39 +1,93 @@
-#' @title Pobieranie danych kontekstowych o uczniach.
+#' @title Pobieranie danych kontekstowych o uczniach
 #' @description
 #' Pobiera z bazy danych zmaterializowany zbiór danych zawierający wszelkie
 #' infromacje kontekstowe potrzebne przy wyliczaniu EWD.
-#' 
+#'
 #' Jeden wiersz zwracanej ramki danych opisuje pierwsze/ostatnie (patrz niżej)
 #' przystąpienie ucznia do wskazanego rodzaju egzaminu, a informacje dotyczące
 #' poszczególnych części egzaminu (na chwilę obecną tylko informacja o byciu
 #' laureatem z danego przedmiotu) znajdują się w kolumnach prefiksowanych
 #' zgodnie z tablicą sl_czesci_egzaminow w bazie.
-#' 
-#' W zależności od wartości parametru \code{wyjscie} pobierane są informacje o
-#' uczniach podchodzących po raz pierwszy (\code{wyjscie == TRUE}) lub po raz
-#' ostatni (\code{wyjscie == FALSE}) do wskazanego rodzaju egzaminu.
-#' 
+#'
 #' Jeśli informacje o szkole, klasie, numerze w dzienniku lub dysleksji nie są
 #' dla danego ucznia zgodne w różnych częściach egzaminu, wtedy dana informacja
 #' (szkoła, klasa, dysleksja) zastępowana jest brakiem danych.
-#' 
+#'
 #' W zależności od poziomu uprawnień połączenia z bazą danych funkcja
 #' automatycznie pobiera lub nie informacje o kodzie klasy i numerze ucznia w
-#' dzienniku.
-#' 
+#' dzienniku oraz o jego wieku, który podawany jest jako liczba ukończonych
+#' miesięcy życia w dniu pisania najwcześniej przeprowadzonej części egzaminu
+#' (w danym roku).
+#'
 #' Dane pobierane są w postaci zmaterializowanej z powodów wydajnościowych -
 #' zapytanie realizujące całą niezbędną funkcjonalnośc po stronie bazy danych
 #' wykonywałoby się wieki.
-#' @param src połączenie z bazą danych IBE zwracane przez ZPD::polacz()
-#' @param rodzajEgzaminu rodzaj egzaminu
+#' @param src połączenie z bazą danych IBE zwracane przez funkcję
+#' \code{\link[ZPD]{polacz}}
+#' @param rodzajEgzaminu ciąg znaków - rodzaj egzaminu
 #' @return data.frame
+#' @details
+#' Oprócz innych informacji funkcja zwraca również zmienne logiczne opisujące,
+#' czy dane osoby należą do "wzorcowych" populacji, wykorzystywanych do
+#' estymacji parametrów modeli skalowania i/lub modeli EWD. Są to zmienne
+#' \code{populacja_we} i \code{populacja_wy}. Członkowie populacji "wzorcowych"
+#' definiowani są jako zdający:
+#' \itemize{
+#'   \item{Którzy uczyli się w szkołach dla młodzieży, nie będących szkołami
+#'         specjalnymi, przyszpitalnymi, itp.}
+#'   \item{Którzy podchodzili do egzaminu:
+#'         \itemize{
+#'           \item{Po raz pierwszy - przy skalowaniu i odnośnie wyników
+#'                 "na wyjściu" w modelach EWD - zmienna \code{populacja_wy}.}
+#'           \item{Po raz ostatni (odnotowany) - odnośnie wyników "na wejściu"
+#'                 w modelach EWD - zmienna \code{populacja_we}.}
+#'         }
+#'        }
+#'   \item{Dodatkowo, tylko dla zmiennej \code{populacja_wy}, a więc
+#'         w odniesieniu do skalowania i wyników "na wyjściu" w modelach EWD,
+#'         populacja "wzorcowa" zawężana jest do zdających:
+#'         \itemize{
+#'           \item{Którzy należą do "głównej" kohorty wiekowej dla danego
+#'                 egzaminu, lub kohort o rok lub dwa lata starszych lub
+#'                 młodszych, tzn.:
+#'                 \itemize{
+#'                   \item{Dla ucznów SP w wieku 124-183 miesięcy.}
+#'                   \item{Dla uczniów gimn. w wieku 160-219 miesięcy.}
+#'                   \item{Dla uczniów LO w wieku 196-255 miesięcy.}
+#'                   \item{Dla uczniów T w wieku 208-267 miesięcy.}
+#'                 }
+#'                 Przy tym informacja o wieku nie jest uwzględniana w przypadku
+#'                 sprawdzianu z lat 2002-2004, egzaminu gimnazjalnego z lat
+#'                 2002-2007 i matury z lat 2002-2008, bo informacje o nim są
+#'                 w tych latach niedostępne.
+#'           }
+#'           \item{W przypadku matury brane są pod uwagę wyłącznie osoby,
+#'                 których kod klasy nie jest liczbą. Wynika to z faktu, że
+#'                 liczbowe oznaczenia klas często wykorzystywane są przez OKE
+#'                 dla oznaczenia osób, które nie były uczniami danej szkoły,
+#'                 ale pisały w niej maturę.}
+#'         }
+#'        }
+#' }
+#' Dodatkowe uwagi:
+#' \itemize{
+#'   \item{Informacje o wieku i klasie zostaną wykorszystane tylko wtedy,
+#'         jeśli przekazane do funkcji połączenie z bazą posiada odpowiednio
+#'         wysoki poziom uprawnień.}
+#'   \item{Przed przystąpieniem do skalowania na populacji "wzorcowej" konieczne
+#'         jest jeszcze usunięcie z danych z wynikami surowymi egzaminów
+#'         punktacji za zadania przypisanej laureatom. Musi to jednak zostać
+#'         zrobinone oddzielnie dla każdej części egzaminu (na podstawie
+#'         zwróconych przez tę funkcję informacji o byciu laureatem).}
+#' }
 #' @import dplyr
 #' @import ZPD
 #' @export
-pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
+pobierz_dane_kontekstowe = function(src, rodzajEgzaminu) {
   stopifnot(
     is.src(src),
-    is.vector(rodzajEgzaminu), is.character(rodzajEgzaminu), length(rodzajEgzaminu) == 1, all(!is.na(rodzajEgzaminu))
+    is.vector(rodzajEgzaminu), is.character(rodzajEgzaminu),
+    length(rodzajEgzaminu) == 1, all(!is.na(rodzajEgzaminu))
   )
 
   # sprawdzanie poziomu uprawnień
@@ -42,13 +96,13 @@ pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
     tmp = tbl(src, sql("SELECT * FROM dane_osobowe.obserwacje LIMIT 1"))
     daneOsobowe = TRUE
   }, silent = TRUE)
-  
+
   # definicje agregatów na poziomie {uczeń, rodzajEgzaminu, rok}
   dotsSummarize = list(
     data       = ~min(data_testu, na.rm = T),
-    id_szkoly  = ~min(id_szkoly, na.rm = T), 
-    dysleksja  = ~all(dysleksja, na.rm = T), 
-    id_szkoly2 = ~max(id_szkoly, na.rm = T), 
+    id_szkoly  = ~min(id_szkoly, na.rm = T),
+    dysleksja  = ~all(dysleksja, na.rm = T),
+    id_szkoly2 = ~max(id_szkoly, na.rm = T),
     dysleksja2 = ~any(dysleksja, na.rm = T)
   )
   dotsMutate = list(
@@ -58,9 +112,9 @@ pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
   dotsSelect = c('-id_szkoly2', '-dysleksja2')
   if(daneOsobowe){
     dotsSummarize = append(dotsSummarize, list(
-      klasa      = ~min(klasa,     na.rm = T), 
+      klasa      = ~min(klasa,     na.rm = T),
       kod_u      = ~min(kod_u,     na.rm = T),
-      klasa2     = ~max(klasa,     na.rm = T), 
+      klasa2     = ~max(klasa,     na.rm = T),
       kod_u2     = ~max(kod_u,     na.rm = T)
     ))
     dotsMutate = append(dotsMutate, list(
@@ -69,12 +123,11 @@ pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
     ))
     dotsSelect = append(dotsSelect, c('-klasa2', '-kod_u2'))
   }
-  
+
   # pobranie danych na poziomie {uczeń, rodzajEgzaminu, czescEgzaminu, rok} i agregacja do {uczeń, rok}
   testy = suppressMessages(
     pobierz_testy(src) %>%
       filter_(~dane_ewd == TRUE, ~rodzaj_egzaminu == rodzajEgzaminu, ~czy_egzamin == TRUE) %>%
-      inner_join(tbl(src, sql('SELECT * FROM sl_czesci_egzaminow'))) %>%
       select_('id_testu', 'prefiks', 'data_testu')
   )
   uczniowieTesty = suppressMessages(
@@ -91,24 +144,25 @@ pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
       mutate_(.dots = dotsMutate) %>%
       select_(.dots = dotsSelect)
   )
-  
+
   # konwersja informacji o byciu laureatem do postaci szerokiej
   laureaci = uczniowieTesty %>%
     mutate_(prefiks = ~paste0('laur_', prefiks)) %>%
     reshape2::dcast(id_obserwacji + rok ~ prefiks, value.var = 'laureat')
-  
+
   # złączenie i usunięcie zbędnych danych
   rm(uczniowieTesty)
   dane = suppressMessages(inner_join(dane, laureaci))
   rm(laureaci)
-  
+
   # dołączenie informacji o szkołach
   szkoly = pobierz_szkoly(src) %>%
-    select_('id_szkoly', 'rok', 'typ_szkoly', 'publiczna', 'specjalna', 'dla_doroslych', 'przyszpitalna', 'artystyczna') %>%
+    select_('id_szkoly', 'rok', 'typ_szkoly', 'publiczna', 'specjalna',
+            'dla_doroslych', 'przyszpitalna', 'artystyczna') %>%
     collect()
   dane = suppressMessages(inner_join(dane, szkoly))
   rm(szkoly)
-  
+
   # dołączanie informacji o obserwacjach
   obserwacje = pobierz_uczniow(src, daneOsobowe = daneOsobowe) %>%
     select_('-id_cke') %>%
@@ -117,33 +171,73 @@ pobierz_dane_kontekstowe = function(src, rodzajEgzaminu){
   rm(obserwacje)
 
   # oznaczamy pierwsze przystąpienia
-  pierwsze = filtruj_przystapienia(src, pierwsze = TRUE, rodzajEgzaminu = rodzajEgzaminu, czescEgzaminu = NULL, czyEwd = TRUE) %>%
+  pierwsze = filtruj_przystapienia(src, pierwsze = TRUE,
+                                   rodzajEgzaminu = rodzajEgzaminu,
+                                   czescEgzaminu = NULL, czyEwd = TRUE) %>%
     collect() %>%
     select_('-rodzaj_egzaminu', '-dane_ewd') %>%
     mutate_(pierwsze = TRUE)
   dane = suppressMessages(left_join(dane, pierwsze))
   rm(pierwsze)
-  
+
   # oznaczamy ostatnie przystąpienia
-  ostatnie = filtruj_przystapienia(src, pierwsze = FALSE, rodzajEgzaminu = rodzajEgzaminu, czescEgzaminu = NULL, czyEwd = TRUE) %>%
+  ostatnie = filtruj_przystapienia(src, pierwsze = FALSE,
+                                   rodzajEgzaminu = rodzajEgzaminu,
+                                   czescEgzaminu = NULL, czyEwd = TRUE) %>%
     collect() %>%
     select_('-rodzaj_egzaminu', '-dane_ewd') %>%
     mutate_(ostatnie = TRUE)
   dane = suppressMessages(left_join(dane, ostatnie))
   rm(ostatnie)
-  
-  # wygenerowanie zmiennej określającej populację
-  dane = dane %>%
-    mutate_(
-      populacja_we = ~publiczna %in% TRUE & specjalna %in% FALSE & dla_doroslych %in% FALSE & przyszpitalna %in% FALSE & ostatnie %in% TRUE,
-      populacja_wy = ~publiczna %in% TRUE & specjalna %in% FALSE & dla_doroslych %in% FALSE & przyszpitalna %in% FALSE & pierwsze %in% TRUE
-    )
-  if(daneOsobowe){
+
+  # ew. wyliczenie wieku w miesiącach
+  if (daneOsobowe) {
     dane = dane %>%
-      mutate(wiek = as.numeric(substr(dane$data, 1, 4)) * 12 + as.numeric(substr(dane$data, 6, 7)) - as.numeric(substr(dane$data_ur, 1, 4)) * 12 - as.numeric(substr(dane$data_ur, 6, 7)))
+      mutate(wiek = as.numeric(substr(dane$data, 1, 4)) * 12 +
+               as.numeric(substr(dane$data, 6, 7)) -
+               as.numeric(substr(dane$data_ur, 1, 4)) * 12 -
+               as.numeric(substr(dane$data_ur, 6, 7)))
   }
   dane = dane %>%
     select_('-data')
-  
+
+  # wygenerowanie zmiennej określającej populację
+  dane = dane %>%
+    mutate_(
+      populacja_we = ~specjalna %in% FALSE & dla_doroslych %in% FALSE &
+        przyszpitalna %in% FALSE & ostatnie %in% TRUE,
+      populacja_wy = ~specjalna %in% FALSE & dla_doroslych %in% FALSE &
+        przyszpitalna %in% FALSE & pierwsze %in% TRUE
+    )
+  if (daneOsobowe) {
+    if (rodzajEgzaminu == "sprawdzian") {
+      wiekWzor = 153.5
+      rokSprawdzWiek = 2008
+    } else if (rodzajEgzaminu == "egzamin gimnazjalny") {
+      wiekWzor = 189.5
+      rokSprawdzWiek = 2005
+    } else {
+      wiekWzor = 225.5
+      rokSprawdzWiek = 2008
+    }
+    dane = dane %>%
+      mutate_(
+        populacja_wy = ~populacja_wy &
+          ifelse(rok >= rokSprawdzWiek,
+                 (wiek - ifelse(typ_szkoly %in% "T", 12, 0)) %in%
+                   seq(wiekWzor - 29.5, wiekWzor + 29.5, 1),
+                 TRUE)
+      )
+    if (rodzajEgzaminu == "matura") {
+      dane = dane %>%
+        mutate_(
+          populacja_wy = ~populacja_wy & grepl("^[^[:digit:]]+$", klasa))
+    }
+  } else {
+    warning("Ze względu na zbyt niski poziom uprawnień dostępu do bazy ",
+            "niemożliwe było wykorzystanie informacji o wieku i kodzie klasy ",
+            "przy tworzeniu zmiennych 'populacja_we' i 'populacja_wy'.")
+  }
+
   return(dane)
 }
---
+++ R/pobierz_wyniki_surowe.R
@@ -0,0 +1,140 @@
+#' @title Pobieranie surowych wyników egzaminu
+#' @description
+#' Funkcja pobiera surowe wyniki egzaminu i zapisuje je na dysku w postaci
+#' plików RData.
+#' \itemize{
+#'   \item{Funkcja sprawdzi, czy w aktywnym katalogu istnieje katalog
+#'         \code{dane surowe}. Jeśli tak, zapisze tam pliki, jeśli nie, najpierw
+#'         go utworzy, a potem zapisze tam pliki.}
+#'   \item{Dane dotyczące wyników z poszczególnych lat zapisane zostaną
+#'         w oddzielnych plikach, o nazwach postaci \code{nazwa egzaminu rok.RData}.}
+#'   \item{Każdy plik w formacie RData będzie zawierał ramki danych z wynikami
+#'         poszczególnych części egzaminu.
+#'         \itemize{
+#'           \item{Nazwy ramek danych odpowiadają prefiksom tych części egzaminu
+#'                 w bazie, w tablic\code{sl_czesci_egzaminow}.}
+#'           \item{Ich wewnętrzna struktura jest taka sama, jak struktura
+#'                 wartości zwracanej przez funkcję pobierz_czesc_egzaminu()
+#'                 z pakietu ZPD.}
+#'           \item{Mają przypisane klasy (oprócz \code{data.frame}):
+#'                 \code{czescEgzaminu} i \code{wynikiSurowe}.}
+#'           \item{Mają przypisany atrybut \code{dataPobrania}, zawierający
+#'                 datę w formie ciągu znaków postaci rrrr-mm-dd.}
+#'         }
+#'        }
+#' }
+#' Dodatkowo, o ile parametr \code{daneKontekstowe} ma wartość \code{TRUE},
+#' w tym samym katalogu zapisany zostanie plik \code{nazwa egzaminu - kontekstowe.RData},
+#' zawierający ramkę danych o nazwie \code{prefiksKontekstowe} (gdzie
+#' \code{prefiks} opisuje rodzaj egzaminu: \code{s} - sprawdzian, \code{g} - egz.
+#' gimn., \code{m} - matura), będącą wynikiem wywołania funkcji
+#' \code{\link{pobierz_dane_kontekstowe}}.
+#' @param rodzajEgzaminu ciąg znaków - rodzaj egzaminu
+#' @param lata NULL lub wektor liczb - lata, dla których mają zostać pobrane
+#' wyniki; jeśli NULL pobrane zostaną wyniki z wszystkich dostępnych lat
+#' @param nadpisz wartość logiczna - jeśli w miejscu, w którym mają być zapisane
+#' wyniki są już pliki z wynikami, to czy je nadpisać? jeśli FALSE, pobrane
+#' zostaną wyniki tylko z lat, dla których pliki jeszcze nie istnieją
+#' @param daneKontekstowe wartość logiczna - czy pobrać również plik z danymi
+#' kontekstowymi (tj. o uczniach i szkołach)?
+#' @param src NULL połączenie z bazą danych IBE zwracane przez funkcję
+#' \code{\link[ZPD]{polacz}}; pozwala posłużyć się połączeniem o wyższych niż
+#' domyślne prawach dostępu, co ma znaczenie dla zakresu pobieranych danych
+#' kontekstowych
+#' @return lista z nazwami zapisanych plików (niewidocznie)
+#' @import ZPD
+#' @export
+pobierz_wyniki_surowe = function(rodzajEgzaminu, lata = NULL, nadpisz = FALSE,
+                                 daneKontekstowe = TRUE, src = NULL) {
+  stopifnot(
+    is.character(rodzajEgzaminu), length(rodzajEgzaminu) == 1,
+    all(rodzajEgzaminu %in% c("sprawdzian", "egzamin gimnazjalny", "matura")),
+    is.numeric(lata) | is.null(lata), length(lata) > 0 | is.null(lata),
+    all(as.integer(lata)) == lata,
+    all(nadpisz %in% c(TRUE, FALSE)), length(nadpisz) == 1,
+    all(daneKontekstowe %in% c(TRUE, FALSE)), length(daneKontekstowe) == 1,
+    is.src(src) | is.null(src)
+  )
+  if (is.null(src)) {
+    src = polacz()
+  }
+  if (is.null(lata)) {
+    lata = pobierz_testy(src) %>%
+      filter_(~rodzaj_egzaminu == rodzajEgzaminu) %>%
+      select(.dots = ~rok) %>%
+      collect %>%
+      unique %>% as.list %>% unlist
+  }
+
+  # sprawdzanie, co jest na dysku
+  if (!dir.exists("dane surowe")) {
+    dir.create("dane surowe")
+    message("Utworzono katalog 'dane surowe' w aktywnym katalogu:\n'", getwd(),"'\n")
+  }
+  czyPobrane = file.exists(paste0("dane surowe/", rodzajEgzaminu, " ", lata, ".RData"))
+  if (any(czyPobrane)) {
+    message("Istnieją już zapisane pliki z wynikami tego egzaminu z lat: ",
+            paste0(lata[czyPobrane], collapse = ", "), ".\n",
+            ifelse(nadpisz,
+                   "Zostaną one nadpisane nowo pobranymi danymi.\n",
+                   "Dane z tych lat nie zostaną pobrane.\n"))
+    if (!nadpisz) {
+      lata = lata[!czyPobrane]
+    }
+  }
+  if (daneKontekstowe) {
+    if (file.exists(paste0("dane surowe/", rodzajEgzaminu,
+                           "- kontekstowe.RData"))) {
+      message("Istnieje już zapisany plik z danymi kontekstowymi. ",
+              "Zostanie on nadpisany nowymi danymi.\n")
+    }
+  }
+
+  message(rodzajEgzaminu)
+  # pobieranie i zapis wyników
+  for (i in lata) {
+    message("\nRok ", i, ":")
+    if ((rodzajEgzaminu == "sprawdzian" & i < 2003) |
+        (rodzajEgzaminu == "egzamin gimnazjalny" & i < 2006) |
+        (rodzajEgzaminu == "matura" & i < 2010)) {
+      czyEwd = FALSE
+    } else {
+      czyEwd = TRUE
+    }
+    czesciEgzaminu = pobierz_testy(src) %>%
+      filter_(~rodzaj_egzaminu == rodzajEgzaminu, ~rok == i) %>%
+      select_(.dots = list(~czesc_egzaminu, ~prefiks)) %>%
+      collect %>%
+      unique
+    for (j in 1:nrow(czesciEgzaminu)) {
+      if (czesciEgzaminu$czesc_egzaminu[j] != "") {
+        message("  ", czesciEgzaminu$czesc_egzaminu[j])
+      }
+      temp = pobierz_wyniki_egzaminu(src, rodzajEgzaminu,
+                                     czesciEgzaminu$czesc_egzaminu[j],
+                                     i, czyEwd) %>%
+        collect
+      class(temp) = append(class(temp), c("wynikiSurowe", "czescEgzaminu"))
+      attributes(temp)$dataPobrania = Sys.time()
+      assign(czesciEgzaminu$prefiks[j], temp)
+      rm(temp)
+    }
+    nazwaPliku = paste0("dane surowe/", rodzajEgzaminu, " ", i, ".RData")
+    save(list = czesciEgzaminu$prefiks, nazwaPliku)
+    message(" zapisano do pliku: ", nazwaPliku)
+  }
+  pliki = paste0("dane surowe/", rodzajEgzaminu, " ", lata, ".RData")
+  # pobieranie i zapis danych kontekstowych
+  if (daneKontekstowe) {
+    message("\nDane o uczniach i szkołach:")
+    temp = pobierz_dane_kontekstowe(src, rodzajEgzaminu)
+    assign(paste0(substr(rodzajEgzaminu, 1, 1), "Kontekstowe"), temp)
+    rm(temp)
+    nazwaPliku = paste0("dane surowe/", rodzajEgzaminu, "- kontekstowe.RData")
+    save(list = paste0(substr(rodzajEgzaminu, 1, 1), "Kontekstowe"), nazwaPliku)
+    message(" zapisano do pliku: ", nazwaPliku)
+    pliki = append(pliki, nazwaPliku)
+  }
+
+  invisible(pliki)
+}
